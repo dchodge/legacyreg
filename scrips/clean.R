b8ab32d07894e4bdab9f3c41bd92ea832321d771
thimotei <timothywilliamrussell@gmail.com>
2022-10-27 16:00:16 +0100

    moving all old scripts to scripts folder

--- scrips/clean.R
+++ scrips/clean.R
@@ -1,123 +1,132 @@
-library(data.table)
-library(lemon)
-library(ggplot2)
+library(tidyverse)
+library(here)
+library(lubridate)
 
-setDTthreads(threads = 8)
+#########################################
+# Find symptom-only infections #
+########################################
+add_symptoms_only <- function(raw_legacy_full) {
+    symptoms.only <- raw_legacy_full %>% 
+        filter(!is.na(episode_number)) %>%
+        group_by(elig_study_id, episode_number) %>%
+        fill(REDCAP_PCR_test_site, .direction = "updown") %>%
+        fill(Sx_severity, .direction = "updown") %>%
+        slice_head() %>%
+        ungroup() %>%
+        filter(str_detect(Sx_severity, "mild") |
+                str_detect(Sx_severity, "moderate") |
+                str_detect(Sx_severity, "severe")) %>%
+        filter(is.na(REDCAP_PCR_test_site))
+    
+    symptoms.only <- symptoms.only %>%
+    mutate(episode_symptoms_only = "symptoms_only")
+    
+    symptoms.only <- symptoms.only %>%
+    select(elig_study_id, episode_number, episode_symptoms_only)
+    
+    raw_legacy_full <- raw_legacy_full %>%
+    left_join(symptoms.only)
+  raw_legacy_full
+}
 
-load("../legacyreg/data/Legacy_DataAnnotatedDateSeries_2022-09-09.Rdata")
+############################################################
+############### Get times of everything into one dataframe
+############################################################
 
-dt_raw <- data.table(chrono.df)
-dt_raw <- dt_raw[, elig_study_id := as.numeric(elig_study_id)]
+get_df_legacy_raw_annot <- load(here::here("data", "Legacy_DataAnnotatedDateSeries_2022-09-09.RData"))
+raw_legacy_full_0909 <- chrono.df
 
-setnames(dt_raw, 
-         c("calendar_date", "elig_study_id", "episode_number",
-           "episode_variant_summarised", "Roche_N_result",
-           "ic50_wildtype", "ic50_Alpha", "ic50_Delta",
-           "ic50_Omicron_BA1", "ic50_Omicron_BA2", "ic50_Omicron_BA5"),
-         c("date", "id", "infection_id",
-           "voc", "anti_n",
-           "wildtype", "alpha", "delta",
-           "omicron_ba1", "omicron_ba2", "omicron_ba5"))
+get_df_legacy_raw_annot <- load(here::here("data", "Legacy_DataAnnotatedDateSeries_2022-10-14.RData"))
+raw_legacy_full_1014 <- chrono.df
 
-# vaccine data.table
-cols_to_keep_vaccines <- c("date", "id",
-                           "dose_1", "dose_2",
-                           "dose_3", "dose_4",
-                           "date_dose_1", "date_dose_2",
-                           "date_dose_3", "date_dose_4")
+raw_legacy_full_0909 <- raw_legacy_full_0909 %>% add_symptoms_only
+raw_legacy_full_1014 <- raw_legacy_full_1014 %>% add_symptoms_only
 
-dt_vax_long_1 <- melt(dt_raw[, ..cols_to_keep_vaccines],
-     measure.vars = c("dose_1", "dose_2", "dose_3", "dose_4"),
-     variable.name = "dose",
-     value.name = "dose_type"
+raw_legacy_full_0909 %>% select(elig_study_id, episode_variant_summarised) %>% unique %>%
+    pull(episode_variant_summarised) %>% table
+raw_legacy_full_1014 %>% select(elig_study_id, episode_variant_summarised) %>% unique %>%
+    pull(episode_variant_summarised) %>% table
+
+raw_legacy_full_0909 %>% filter(episode_variant_summarised == "Omicron-BA.5") %>% 
+    pull(ic50_Omicron_BA5) %>% `>`(0) %>% sum(na.rm = TRUE)
+raw_legacy_full_1014 %>% filter(episode_variant_summarised == "Omicron-BA.5") %>%
+    pull(ic50_Omicron_BA5) %>% `>`(0) %>% sum(na.rm = TRUE)
+
+raw_legacy_full_0909 %>% pull(ic50_Omicron_BA5) %>% `>`(0) %>% sum(na.rm = TRUE)
+raw_legacy_full_1014 %>% pull(ic50_Omicron_BA5) %>% `>`(0) %>% sum(na.rm = TRUE)
+
+raw_legacy_full <- raw_legacy_full_1014
+
+legacy_ids_all <- raw_legacy_full %>% pull(elig_study_id) %>% unique
+
+
+  
+#####################
+# Get bleed times #
+####################
+bleed_times_full <- left_join(
+    raw_legacy_full %>% arrange(elig_study_id) %>% filter(!is.na(visit_date)) %>%
+        select(elig_study_id, calendar_date) %>% group_by(elig_study_id) %>% unique %>% mutate(info1 = row_number(), type = "bleed_time") %>%
+        select(elig_study_id, calendar_date, type, info1),
+
+    raw_legacy_full %>% arrange(elig_study_id) %>% filter(!is.na(visit_date)) %>% 
+        select(elig_study_id, calendar_date, ic50_wildtype, ic50_Alpha, ic50_Delta, ic50_Omicron_BA1, ic50_Omicron_BA2, ic50_Omicron_BA5) %>% 
+        pivot_longer(!c(elig_study_id, calendar_date), names_to = "info2", values_to = "info3") 
 )
 
-dt_vax_long_2 <- melt(dt_vax_long_1,
-                      measure.vars = c("date_dose_1", "date_dose_2",
-                                       "date_dose_3", "date_dose_4"),
-                      variable.name = "dose_tmp",
-                      value.name = "dose_date"                 
-)
+bleed_times_full <- bleed_times_full %>% mutate(info3 = log2(info3 / 5)) %>%
+    mutate(info3 = case_when(   
+    info3 == 10 ~ 7,   
+    info3 > 3 ~ info3 - 2,
+    info3 == 1 ~ 1,
+    info3 == 0 ~ 1,
+    info3 == 10 ~ 7
+    )
+    ) %>% 
+    mutate(color =  case_when( 
+            info3 == 1 ~ "censored",
+            info3 == 7 ~ "censored",
+            TRUE~ "true",
+        )
+    )
 
-dt_vax_long_2[order(id, dose)]
+#########################################
+# Get ROCHE-N results #
+########################################
 
-dt_vax <- setcolorder(dt_vax_long_2, "dose_date")[,
-  `:=` (date = NULL, dose_tmp = NULL)
-  ][
-    !is.na(dose_date)][order(id, dose)
-  ] %>%
-  unique()
+roche_times_full <- raw_legacy_full %>% arrange(elig_study_id) %>% filter(!is.na(Roche_N_result)) %>%
+    select(elig_study_id, calendar_date, info2 = Roche_N_result) %>% group_by(elig_study_id) %>%
+    mutate(info1 = row_number(), type = "roche_result") %>%
+    select(elig_study_id, calendar_date, type, info1, info2)
 
-# anti-N data.table
+#########################################
+# Get vaccination times #
+########################################
 
-# titre data.table
-cols_to_keep_titres <- c("date", "id", "age", "infection_id",
-                         "voc", "episode_start",
-                         "wildtype", "alpha", "delta",
-                         "omicron_ba1", "omicron_ba2", "omicron_ba5")
+vac_inter <- raw_legacy_full %>% arrange(elig_study_id) %>% 
+    select(elig_study_id, date_dose_1, date_dose_2, date_dose_3, date_dose_4, dose_1, dose_2, dose_3, dose_4) %>% unique 
 
+vac_times_full <- vac_inter %>% select(elig_study_id, date_dose_1, date_dose_2, date_dose_3, date_dose_4) %>% 
+    pivot_longer(!elig_study_id, names_to = "info1", values_to = "calendar_date") %>% mutate(info1 = substr(info1, 6, 11)) %>%
+left_join(
+    vac_inter %>% select(elig_study_id, dose_1, dose_2, dose_3, dose_4) %>% 
+        pivot_longer(!elig_study_id, names_to = "info1", values_to = "info2") 
+) %>% mutate(type = "vaccine_dose") %>% select(elig_study_id, calendar_date, type, info1, info2) %>%
+    mutate(info1 = as.numeric(substr(info1, 6, 6)))
 
-vocs <- c("wildtype", "alpha", "delta",
-          "omicron_ba1", "omicron_ba2", "omicron_ba5")
+#########################################
+# Get known infection times (PCR) #
+########################################
+inf_times_full <- raw_legacy_full %>% arrange(elig_study_id) %>% filter(!is.na(episode_start)) %>%
+    select(elig_study_id, calendar_date = episode_start, info1 = episode_number, info2 = episode_variant_summarised, episode_symptoms_only) %>% unique %>% 
+    filter(is.na(episode_symptoms_only)) %>%
+    group_by(elig_study_id) %>% mutate(info1 = row_number()) %>%
+    mutate(type = "infection") %>% select(elig_study_id, calendar_date, type, info1, info2) 
 
-dt_titres <- dt_raw[, ..cols_to_keep_titres]
+# Combine!!!
+everything_times <- bind_rows(bleed_times_full, roche_times_full, vac_times_full, inf_times_full)
 
-dt_titres_long <- melt(dt_titres, 
-                measure.vars = vocs,
-                variable.name = "titre_type",
-                value.name = "titre"
-)[order(id, date, titre_type)]
-
-dt_titres_trim <- dt_titres_long[!is.na(titre)]
-        
-dt_titres_trim[titre < 4000] %>% 
-  ggplot() + 
-  geom_point(aes(x = date, y = titre, colour = titre_type), alpha = 0.05) +
-  geom_smooth(aes(x = date, y = titre, colour = titre_type)) +
-  # geom_line(aes(x = date, y = titre, colour = titre_type))
-  facet_wrap(~titre_type) +
-  theme_minimal()
-
-
-# trying filtering on original format
-# titre data.table
-cols_to_keep_raw <- c("date", "id", "age", "infection_id",
-                      "voc", "episode_start",
-                      "dose_1", "dose_2",
-                      "dose_3", "dose_4",
-                      "date_dose_1", "date_dose_2",
-                      "date_dose_3", "date_dose_4",
-                      "wildtype", "alpha", "delta",
-                      "omicron_ba1", "omicron_ba2", "omicron_ba5",
-                      "anti_n")
-
-
-dt_trim <- dt_raw[, ..cols_to_keep_raw][order(id, date)]
-
-dt_long <- melt(dt_trim, 
-                measure.vars = vocs,
-                variable.name = "titre_type",
-                value.name = "titre"
-)[order(id, date, titre_type)][!is.na(titre)]
-
-# adding number of bleeds
-dt_long[, bleed_number := .N, by = c("id", "titre_type")]
-
-dt_tmp <- dt_long[id %in% 1:20 & bleed_number > 3][anti_n == "positive"][, .SD[date == min(date)], by = id]
-
-dt_long[id %in% 1:20 & bleed_number > 3] %>% 
-  ggplot() + 
-  # geom_point(aes(x = date, y = titre, colour = titre_type), alpha = 0.05) +
-  # geom_smooth(aes(x = date, y = titre, colour = titre_type)) +
-  geom_line(aes(x = date, y = titre, colour = titre_type)) + 
-  geom_vline(aes(xintercept = date_dose_1), linetype = "dashed") + 
-  geom_vline(aes(xintercept = date_dose_2), linetype = "dashed") + 
-  geom_vline(aes(xintercept = date_dose_3), linetype = "dashed") + 
-  geom_vline(aes(xintercept = date_dose_4), linetype = "dashed") + 
-  geom_vline(data = dt_tmp,
-    aes(xintercept = date), colour = "red", linetype = "dashed") + 
-  facet_rep_wrap(~id) +
-  theme_minimal()
-
-
-
+# People with odd entries people
+vac_data_problem_id <- c("212", "341", "348", "358", "376", "717", "713", "707", "712", "715")
+everything_times <- everything_times %>% filter(!elig_study_id %in% vac_data_problem_id)
+save(everything_times, file = "data/df/everything_long.RData")
